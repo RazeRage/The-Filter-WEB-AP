97b59d2056a8a93b8b0d673054ff2980de7c9d10
RazeRage <90508604+RazeRage@users.noreply.github.com>
2022-05-07 16:53:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function preload() {
 }
+
 function setup() {
     canvas = createCanvas(300 , 300)
     canvas.center()
@@ -9,6 +10,17 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded)
     poseNet.on('pose', gotposes)
 }
+function modelLoaded() {
+    console.log('poseNet is Initiaized')
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results)
+        console.log("nose x =" + results[0].pose.nose.x)
+        console.log("nose y =" + results[0].pose.nose.y)
+    }
+}
 function draw() {
     image(video,0,0,300,300)
 }
@@ -16,3 +28,4 @@ function draw() {
 function take_snapshot() {
     save("MySnap.png")
 }
+
